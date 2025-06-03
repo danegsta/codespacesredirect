99f18d9ac85f00d80d816bc23775f7f370e9b87f
David Negstad <David.Negstad@microsoft.com>
2025-06-02 18:30:28 -0700

    Minimal node.js repro

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace codespacesredirect.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
